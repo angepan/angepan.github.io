cc46c3f132a946f0739aa74be3f409142ae2f012
Angela Pan <angepan@tangistudents.org>
2018-12-17 04:34:33 +0000

    circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -24,103 +24,86 @@ var init = function (window) {
         var circles = [];
 
         // TODO 2 : Create a function that draws a circle  //
-        var drawCircle;
         var drawCircle = function() {
-    circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-    physikz.addRandomVelocity(circle, canvas);
-    view.addChild(circle);
-    circles.push(circle);
-        
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        };
 
         // TODO 3 : Call the drawCircle function 5 times //
-    drawCircle("circle1", "circle2", "circle3", "circle4", "circle5")
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
 
         // TODO 7 : Create a Loop to call drawCircle 100 times
+        
         var counter = 0; 
-        while (counter < 10) {
+        while (counter < 100) {
+            drawCircle();
         // do something
-        counter++
+        counter++;
         }
 
-for (var counter = 0; counter < 10; counter++) {
-    // do something
-    
         view.addChild(fps);
         app.addUpdateable(fps);
     
         runner.checkCircleBounds = function(circle) {
             // TODO 5 : YOUR CODE STARTS HERE //////////////////////
-            
+          
             // if the circle has gone out the right side of the screen then place it off-screen left
-            if ( circle.x > canvas.width + circle.radius ) {
-                circle.x = 0 - circle.radius;
+            if ( circle.x > canvas.width) {
+                circle.x = 0;
             } 
             // TODO 5a) if the circle has gone out of the left side of the screen then place it off-screen right
-            else if ( / * Fill me in! * / ) {
-             if (circle.x > canvas.width) {
-	            circle.x = 0;
-}	   
+            else if (circle.x < 0) {
+	            circle.x = canvas.width;
             } 
 
             // TODO 5b) if the circle has gone out of the top side of the screen then place it off-screen bottom
-            if ( / * Fill me in! * / ) {
-                if (circle.x > canvas.height) {
-	            circle.x = 0;
-}	 
+                if (circle.y > canvas.height) {
+	            circle.y = 0;
+	 
             }
             // TODO 5c) if the circle has gone out of the bottom side of the screen then place it off-screen top 
-            else if ( / * Fill me in! * / ) {
-                if (circle.x > circle.y) {
-	            circle.x = 0;
+            
+            else if (circle.y < 0) {
+	            circle.y = canvas.height;
 }	
-            }
+            
+        };
             // YOUR TODO 5 CODE ENDS HERE //////////////////////////
-        }
     
         var update = function() {
             // TODO 4 : Update the circle's position //
-             physikz.updatePosition(circles[0]);
+             
+             
             // TODO 6 : Call checkCircleBounds on your circles.
-           var counter = 0; 
-            while (counter < 10) {
-            // do something
-            counter++
-            }       
-
-            for (var counter = 0; counter < 10; counter++) {
-            // do something
-            }
-
+         
+          
             // TODO 8 : Iterate over the array
-           var drawCircle = ['Circle1', 'Circle2', 'Circle3', 'Circle4', 'Circle5'];
-           var drawCircle;
+           
            // for loop version
-           for (var i = 0; i < drawCircle.length; i++) {
-           drawCircle = drawCircle[i];
-           console.log(drawCircle);
+           for (var i = 0; i < circles.length; i++) {
+            physikz.updatePosition(circles[i]);
+            console.log(circles[i]);
+            runner.checkCircleBounds(circles[i]);
            }
-
-          // while loop version
-          var i = 0;
-          while (i < drawCircle.length) {
-          drawCircle = drawCricle[i];
-          console.log(drawCricle);
-          i++;
-}
-
-        }
         
+        };
         ////////////////////////////////////////////////////////////////////
         // NO CODE BELOW HERE                                             //
         ////////////////////////////////////////////////////////////////////
         
         runner.circle = circle;
         runner.circles = circles;
-        runner.drawCircle = drawCircle;
+        runner.drawCircle = circles;
         runner.update = update;
         
         app.addUpdateable(window.opspark.runner);
-    }
+    };
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
